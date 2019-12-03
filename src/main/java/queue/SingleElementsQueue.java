c06c47ec81d4252c5e1788d25fff0d0fc3f260c7
Dmitrii_Ananev <dmitrii_ananev@epam.com>
2019-12-03 19:12:02 +0300

    SingleElementsQueue class was created. Main class was added. .gitignore was created. pom.xml was added.

---
+++ src/main/java/queue/SingleElementsQueue.java
@@ -0,0 +1,55 @@
+package queue;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SingleElementsQueue<T> {
+
+    private volatile T element;
+
+    private final ReentrantLock lock;
+
+    private final Condition notEmpty;
+
+    public SingleElementsQueue(){
+        lock = new ReentrantLock();
+        notEmpty = lock.newCondition();
+    }
+
+    private static void checkNotNull(Object v) {
+        if (v == null)
+            throw new NullPointerException();
+    }
+
+    public boolean offer(T e) throws InterruptedException {
+        checkNotNull(e);
+        final ReentrantLock lock = this.lock;
+        lock.lock();
+        try {
+            if (element == null){
+                element = e;
+                return true;
+            } else {
+                notEmpty.await();
+                return false;
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T poll() {
+        final ReentrantLock lock = this.lock;
+        lock.lock();
+        try {
+            T e = element;
+            element = null;
+            return e;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+
+}
